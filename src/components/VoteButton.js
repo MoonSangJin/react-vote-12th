28c0dbfd864bd787e315ce1858e2938a18bbac4b
MoonSangJin <1128msj@naver.com>
2020-11-12 23:01:52 +0900

    complete

---
+++ src/components/VoteButton.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  width: 100px;
+`;
+export default function VoteButton({ vote }) {
+  return <Button onClick={vote}>투표</Button>;
+}
--- src/components/VoteScreen.js
+++ src/components/VoteScreen.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Title from './Title';
-
+import VoteButton from './VoteButton';
+import VoteRow from './VoteRow';
+import axios from 'axios';
 const Wrapper = styled.div`
   height: 100vh;
 `;
 export default function VoteScreen() {
+  const [candidates, setCandidates] = useState(null);
+  const getData = async () => {
+    try {
+      console.log('start');
+      const result = await axios.get(
+        'http://ec2-3-34-5-220.ap-northeast-2.compute.amazonaws.com:2020/candidates'
+      );
+      console.log(result.data);
+
+      result.data = result.data.sort((a, b) => {
+        return b.voteCount - a.voteCount;
+      });
+
+      setCandidates(result.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [candidates]);
+
   return (
     <Wrapper>
       <Title title={'13기 프론트엔드 팀장은 누구?'} fontSize="50" />
-      <Title title={'CEOS 프론트엔드 13기 개발팀장 투표 창입니다'} />
+      <Title
+        title={'CEOS 프론트엔드 13기 개발팀장 투표 창입니다'}
+        fontSize="30"
+      />
+      {candidates && //와;;
+        candidates.map((candidate, index) => {
+          return (
+            <VoteRow key={index} candidate={candidate} order={index}></VoteRow>
+          );
+        })}
     </Wrapper>
   );
 }
